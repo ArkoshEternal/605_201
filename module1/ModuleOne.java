38e773e3d1463e08f6f8adf5b135a3537a5683a6
ArkoshEternal <duncanparke@proton.me>
2025-01-26 14:33:00 -0500

    initial commit of working code and MD start for 605.201 first project

---
+++ module1/ModuleOne.java
@@ -0,0 +1,41 @@
+
+/** 
+ * Assignment One
+ * 
+ * This program prints to the standard output an ascii-art of the word java, 
+ * whose letters are comprised of the letters of the word. e.g. the j will 
+ * be made of ascii js: 
+ *     
+ *         J    A    V     V    A   
+ *         J   A A    V   V    A A  
+ *     J   J  AAAAA    V V    AAAAA 
+ *      J J  A     A    V    A     A
+ * 
+ * @author: Duncan Parke
+ */
+
+public class ModuleOne 
+{
+    /**
+     * Program Entry Point
+     */
+    public static void main( String [] args) 
+    {
+        // Display JAVA in ascii art
+        System.out.println(); // Newline
+        System.out.println(); // Newline
+        System.out.println(); // Newline
+        System.out.println(); // Newline
+        System.out.println(); // Newline
+        System.out.println(); // Newline
+        System.out.println(); // Newline 
+        System.out.println("    J    A    V     V    A   ");
+        System.out.println("    J   A A    V   V    A A  ");
+        System.out.println("J   J  AAAAA    V V    AAAAA "); 
+        System.out.println(" J J  A     A    V    A     A"); 
+        System.out.println(); // Newline
+        System.out.println(); // Newline
+        System.out.println(); // Newline
+    } // end main()
+    
+} // end ModuleOne
